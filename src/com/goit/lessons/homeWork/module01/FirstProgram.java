019c24ef20049ea63274cb0462ce42614465213f
Oksana <pasom@bigmir.net>
2016-05-28 14:51:48 +0400

    fist commit via idea

---
+++ src/com/goit/lessons/homeWork/module01/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.goit.lessons.homeWork.module01;
+
+/**
+ * Created by Оксана on 28.05.2016.
+ */
+public class FirstProgram {
+        public static void main (String[] args){
+            System.out.print("Hi");
+        };
+    }
+
