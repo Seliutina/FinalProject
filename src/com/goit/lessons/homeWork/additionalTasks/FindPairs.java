89a5cc35ca4a497ea95e2266293ff983ca53a561
Oksana <pasom@bigmir.net>
2016-06-21 09:47:26 +0400

    OptionalTasks

---
+++ src/com/goit/lessons/homeWork/additionalTasks/FindPairs.java
@@ -0,0 +1,32 @@
+package com.goit.lessons.homeWork.additionalTasks;
+
+public class FindPairs {
+
+    public void findPairs(int[] intArray,int target){
+
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = i; j < intArray.length; j++) {
+                if((intArray[i] + intArray[j]) == target){
+                    System.out.println(intArray[i] + " " + intArray[j]);
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public int[] array(){
+        int[] array = {1, 12, 3, 4, -5, 6};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("  " + array[i]);
+        }
+        System.out.println();
+        return array;
+    }
+
+    public static void main (String args[]) {
+
+        FindPairs findPairs = new FindPairs();
+
+        findPairs.findPairs(findPairs.array(), 7);
+    }
+}
