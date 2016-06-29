af4c278e015d50c82134f57b5a4861c8a6dce208
Oksana <pasom@bigmir.net>
2016-06-29 22:27:27 +0400

    TryToUseCollection

---
+++ src/com/goit/lessons/homeWork/additionalTasks/collectionUsage/findPairs/FindPairsCollection.java
@@ -0,0 +1,6 @@
+Unstaged changes after reset:
+M	src/com/goit/examplesFromShildt/strings/CommandLineArguments.java
+D	src/com/goit/lessons/homeWork/additionalTasks/collectionUsage/ArrayListSizeException.java
+D	src/com/goit/lessons/homeWork/additionalTasks/collectionUsage/FindPairsCollection.java
+M	src/com/goit/lessons/homeWork/additionalTasks/collectionUsage/findPairs/FindPairsCollection.java
+M	src/com/goit/lessons/homeWork/module01/FirstProgram.java
