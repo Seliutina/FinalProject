e722eb1ed98fe812b4ff18b7c4534d43de741e55
Oksana <pasom@bigmir.net>
2016-07-12 05:46:33 +0400

    SomeExample

---
+++ src/com/goit/examplesFromShildt/streams/WriteMethod.java
@@ -0,0 +1,14 @@
+package com.goit.examplesFromShildt.streams;
+
+/**
+ * p. 363
+ */
+public class WriteMethod {
+    public static void main(String[] args) {
+        int b;
+
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
