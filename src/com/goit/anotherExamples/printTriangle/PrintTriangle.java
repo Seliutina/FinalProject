44d6ad867813b14c91cfc907d58325befed8fe4e
Oksana <pasom@bigmir.net>
2016-07-14 03:47:54 +0400

    SomeExample

--- src/com/goit/anotherExamples/printTriangle/PrintTriangle.java
+++ src/com/goit/anotherExamples/printTriangle/PrintTriangle.java
@@ -4,6 +4,10 @@ package com.goit.anotherExamples.printTriangle;
  * Let’s print a triangle made of asterisks (‘*’) separated by spaces. The triangle
  should consist of n rows, where n is a given positive integer, and consecutive rows should
  contain 1, 2, . . . , n asterisks. For example, for n = 4 the triangle should appear as follows:
+ *
+ * *
+ * * *
+ * * * *
  */
 public class PrintTriangle {
     public static void main(String[] args) {
