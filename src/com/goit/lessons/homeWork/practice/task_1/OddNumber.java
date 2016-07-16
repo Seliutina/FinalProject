74d0f36c4f6fcfd511ffeaed4ee8f28b0e091209
Oksana <pasom@bigmir.net>
2016-07-16 04:12:58 +0400

    Task_1.3

--- src/com/goit/lessons/homeWork/practice/task_1/OddNumber.java
+++ src/com/goit/lessons/homeWork/practice/task_1/OddNumber.java
@@ -6,16 +6,18 @@ public class OddNumber {
         int counter = 0;
         for (int i = 0; i < A.length; i++){
             if(A[i]%2 != 0){
-                k = i; counter += counter; break;
+                k = i;
+                counter = counter + 1;
+                break;
             }
         }
-        if (counter != 0) return k;
-        else return -1;
+        if (counter != 0) {return k;}
+        else {return -1;}
     }
 
     public static void main(String[] args) {
         OddNumber oddNumber = new OddNumber();
-        int[] A = {2,4,6,8,4,2};
+        int[] A = {10,4,8,8,4,2};
         System.out.println("The first odd number index in the array is " + oddNumber.oddNumber(A));
     }
 }
